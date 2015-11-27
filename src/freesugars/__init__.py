543a5cb66db0120009b2d7e8d91b347e10817d52
Michael Platings <michael@platin.gs>
2015-11-27 22:52:36 +0000

    very basic test for products with no ingredients with free sugars

--- src/freesugars/__init__.py
+++ src/freesugars/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "0.1.0"
+
+
+def free_sugars(ingredients={}, kilojoules=0):
+    return 0
--- tests/test_freesugars.py
+++ tests/test_freesugars.py
@@ -1,6 +1,14 @@
-
+from __future__ import absolute_import, print_function
+import unittest
+from freesugars import free_sugars
 from freesugars.__main__ import main
 
+class TestFreeSugars(unittest.TestCase):
 
-def test_main():
-    assert main([]) == 0
+    def test_no_ingredients(self):
+        self.assertEqual(0, free_sugars(ingredients={}, kilojoules=0))
+        self.assertEqual(0, free_sugars(ingredients={}, kilojoules=100))
+
+    def test_main(self):
+        self.assertEqual(0, main([]))
+
